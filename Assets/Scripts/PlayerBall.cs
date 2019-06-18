c02a473281162bbb4b7c65a11de955b21d2dd155
yamakentoc <yamakentoc@gmail.com>
2019-06-18 18:31:06 +0900

    ボールを取得するとmassを大きくするように

--- Assets/Scripts/PlayerBall.cs
+++ Assets/Scripts/PlayerBall.cs
@@ -47,23 +47,25 @@ public class PlayerBall : MonoBehaviour {
         //rigidbody.AddForce(transform.forward * speed, ForceMode.Force);
         rigidbody.AddForce(5 * (transform.forward * speed - rigidbody.velocity));
         rigidbody.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, radian, 0), 10);
+        Debug.Log("velocity.magnitude: " + rigidbody.velocity.magnitude);
     }
 
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject.tag.Equals("NeutralBall")) {
-            //transform.localScale = new Vector3(transform.localScale.x + 0.05f,
-            //                                   transform.localScale.y + 0.05f,
-            //                                   transform.localScale.z + 0.05f);
+            transform.localScale = new Vector3(transform.localScale.x + 0.05f,
+                                               transform.localScale.y + 0.05f,
+                                               transform.localScale.z + 0.05f);
 
             //デバッグ
-            transform.localScale = new Vector3(transform.localScale.x * 1.1f,
-                                               transform.localScale.y * 1.1f,
-                                               transform.localScale.z * 1.1f);
+            //transform.localScale = new Vector3(transform.localScale.x * 1.1f,
+            //                                   transform.localScale.y * 1.1f,
+            //                                   transform.localScale.z * 1.1f);
 
             transform.localPosition = new Vector3(transform.localPosition.x,
                                               transform.localScale.y / 2,
                                               transform.localPosition.z);
 
+            rigidbody.mass = transform.localScale.x;
             cameraController.SetMovePosition((transform.localScale - previousScale) * 2.0f);
             Destroy(other.gameObject);
             previousScale = transform.localScale;

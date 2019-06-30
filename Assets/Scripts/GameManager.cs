3d881ec7a55ddb71d0475420df664c118206f682
yamakentoc <yamakentoc@gmail.com>
2019-06-30 16:03:56 +0900

    ボールのマテリアルをランダムに変更

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,14 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class GameManager : MonoBehaviour {
 
-    [SerializeField] GameObject player, enemy;
+    [SerializeField] GameObject player, enemyPrefab;
     [SerializeField] GameObject neutralBall, neutralBalls;
+    [SerializeField] List<Material> ballMaterials;
+    private GameObject playerBall; 
     private const int SUM_PLAYER = 8;
 
     void Start() {
+        playerBall = player.transform.GetChild(0).gameObject;
+        
         DeployPlayerBall();
         DeployNeutralBall();
     }
@@ -24,13 +29,17 @@ public class GameManager : MonoBehaviour {
     }
 
     private void DeployPlayerBall() {
+        ballMaterials = ballMaterials.OrderBy(a => System.Guid.NewGuid()).ToList();
         int playerPosition = Random.Range(0, 8);
         for (int i = 0; i < SUM_PLAYER; i++) {
             Vector3 position = Quaternion.Euler(0f, 360f / SUM_PLAYER * i, 0f) * transform.forward * 75f;
             if (i == playerPosition) {
                 player.transform.position = position;
+                playerBall.GetComponent<Renderer>().material.color = ballMaterials[i].color;
             } else {
-                GameObject enemyBall = Instantiate(enemy, position, Quaternion.identity);
+                GameObject enemy = Instantiate(enemyPrefab, position, Quaternion.identity);
+                GameObject enemyBall = enemy.transform.GetChild(0).gameObject;
+                enemyBall.GetComponent<Renderer>().material.color = ballMaterials[i].color;
             }
         }
     }

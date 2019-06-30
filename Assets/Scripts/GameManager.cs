418c3ec427162b3da679c6e1a80616e704a4298a
yamakentoc <yamakentoc@gmail.com>
2019-06-30 14:58:09 +0900

    円状にプレイヤーと敵を配置するように

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+    [SerializeField] GameObject player, enemy;
     [SerializeField] GameObject neutralBall, neutralBalls;
+    private const int SUM_PLAYER = 8;
 
     void Start() {
+        DeployPlayerBall();
+        DeployNeutralBall();
+    }
+
+    private void DeployNeutralBall() {
         int value = 0;
         while (value < 1000) {
             value++;
@@ -15,5 +22,18 @@ public class GameManager : MonoBehaviour {
             Instantiate(neutralBall, position, Quaternion.identity, neutralBalls.transform);
         }
     }
+
+    private void DeployPlayerBall() {
+        int playerPosition = Random.Range(0, 8);
+        for (int i = 0; i < SUM_PLAYER; i++) {
+            Vector3 position = Quaternion.Euler(0f, 360f / SUM_PLAYER * i, 0f) * transform.forward * 75f;
+            if (i == playerPosition) {
+                player.transform.position = position;
+            } else {
+                GameObject enemyBall = Instantiate(enemy, position, Quaternion.identity);
+            }
+        }
+    }
+
 
 }
